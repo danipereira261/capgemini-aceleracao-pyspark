72701d33b749abc5ebe8095a6763e5c074086b68
Daniele Pereira <dpereira261@gmail.com>
2022-04-11 12:09:14 -0300

    Adicionado o schema online-retail e census-income

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -1,7 +1,21 @@
+from datetime import datetime
+from sqlite3 import Timestamp
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, TimestampType
+
+schema_online_retail = StructType([
+	StructField("InvoiceNo", StringType(), True),
+	StructField("StockCode", StringType(), True),
+	StructField("Description", StringType(), True),
+	StructField("Quantity", IntegerType(), True),
+	StructField("InvoiceDate", TimestampType(), True),
+	StructField("UnitPrice", FloatType(), True),
+	StructField("CustomerID", IntegerType(), True),
+	StructField("Country", StringType(), True)
+])
+
  
 if __name__ == "__main__":
 	sc = SparkContext()
@@ -10,6 +24,6 @@ if __name__ == "__main__":
 	df = (spark.getOrCreate().read
 		          .format("csv")
 		          .option("header", "true")
-		          #.schema(schema_online_retail)
+		          .schema(schema_online_retail)
 		          .load("/home/spark/capgemini-aceleracao-pyspark/data/online-retail/online-retail.csv"))
 	print(df.show())
