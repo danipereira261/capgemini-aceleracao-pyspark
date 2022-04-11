a3542f05176a6c00322fcafd76a965218f4f9166
Daniele Pereira <dpereira261@gmail.com>
2022-04-11 16:50:15 -0300

    Adicionado as validacoes de Qualidade

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from sqlite3 import Timestamp
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
@@ -10,11 +9,58 @@ schema_online_retail = StructType([
 	StructField("StockCode", StringType(), True),
 	StructField("Description", StringType(), True),
 	StructField("Quantity", IntegerType(), True),
-	StructField("InvoiceDate", TimestampType(), True),
-	StructField("UnitPrice", FloatType(), True),
+	StructField("InvoiceDate", StringType(), True),
+	StructField("UnitPrice", StringType(), True),
 	StructField("CustomerID", IntegerType(), True),
 	StructField("Country", StringType(), True)
 ])
+
+def qa_InvoiceNo(df):
+	df = df.withColumn("qa_InvoiceNo", 
+	F.when(F.col("InvoiceNo").startswith("C"), "C")
+	 .when(F.col("InvoiceNo").rlike("^[0-9]*$"), "OK").otherwise("F"))
+	print(df.groupBy("qa_InvoiceNo").count().distinct().orderBy("qa_InvoiceNo", ascending=False).show())
+
+def qa_StockCode(df):
+	df = df.withColumn("qa_StockCode", 
+	F.when(~F.col("StockCode").rlike("([0-9a-zA-Z]{5})"), "F")
+	 .otherwise("OK"))
+	print(df.groupBy("qa_StockCode").count().distinct().orderBy("qa_StockCode", ascending=False).show())
+
+def qa_Description(df):
+	df = df.withColumn("qa_Description", 
+	F.when(F.col("Description").isNull(), "M")
+	 .when(F.col("Description") == "", "M")
+	 .otherwise("OK"))
+	print(df.groupBy("qa_Description").count().distinct().orderBy("qa_Description", ascending=False).show())
+
+def qa_Quantity(df):
+	df = df.withColumn("qa_Quantity", 
+	F.when(~F.col("Quantity").rlike("\d"), "N")
+	 .otherwise("OK"))
+	print(df.groupBy("qa_Quantity").count().distinct().orderBy("qa_Quantity", ascending=False).show())		
+
+def qa_InvoiceDate(df):
+	df = df.withColumn("InvoiceDate", 
+	F.col("InvoiceDate").cast(TimestampType()))
+	print(df.printSchema())
+
+def qa_UnitPrice(df):
+	df = df.withColumn("UnitPrice", 
+	F.col("UnitPrice").cast(FloatType()))
+	print(df.printSchema())
+
+def qa_CustomerID(df):
+	df = df.withColumn("qa_CustomerID", 
+	F.when(~F.col("CustomerID").rlike("([0-9a-zA-Z]{5})"), "F").otherwise("OK"))
+	print(df.groupBy("qa_CustomerID").count().distinct().orderBy("qa_CustomerID", ascending=False).show())
+
+def qa_Country(df):
+	df = df.withColumn("qa_Country", 
+	F.when(F.col("Country").isNull(), "M")
+	 .when(F.col("Country") == "", "M")
+	 .otherwise("OK"))
+	print(df.groupBy("qa_Country").count().distinct().orderBy("qa_Country", ascending=False).show()) 		
 
  
 if __name__ == "__main__":
@@ -26,4 +72,17 @@ if __name__ == "__main__":
 		          .option("header", "true")
 		          .schema(schema_online_retail)
 		          .load("/home/spark/capgemini-aceleracao-pyspark/data/online-retail/online-retail.csv"))
-	print(df.show())
+
+	qa_InvoiceNo(df)
+	qa_StockCode(df)
+	qa_Description(df)
+	qa_Quantity(df)
+	qa_InvoiceDate(df)
+	qa_UnitPrice(df)
+	qa_CustomerID(df)
+	qa_Country(df)			  
+
+	
+	
+
+	
